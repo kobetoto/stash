b08f1098c85325977df6fceda81519477907206e
Thomas <106164290+kobetoto@users.noreply.github.com>
2024-12-13 16:12:37 +0100

    strjoin fix for free

--- getNext/get_next_line_utils.c
+++ getNext/get_next_line_utils.c
@@ -1,46 +1,60 @@
 #include "get_next_line.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-
-char *check_and_cut(char *s) 
+char	*ft_strjoin(char  *cpy, char *buff)
 {
-    int i;
-    char *rslt;
-    char *nl;
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	total_len;
+	char			*s_join;
 
-    rslt = malloc(sizeof(char) * );//...........malloc!
-    if (!rslt)
-        return (NULL);
-
-    nl = ft_strchr(s, '\n');
-    if (nl == NULL) 
-	{
-        free(rslt);
-        return (s);
+    if(!cpy)
+    {
+        return (buff);
     }
+	total_len = ft_strlen(cpy) + ft_strlen(buff);
+	s_join = malloc(sizeof(char) * total_len +1);
+	if (s_join == NULL)
+		return (NULL);
 	i = 0;
-    while (&s[i] != nl) 
+	j = 0;
+	while (cpy[i] != '\0')
 	{
-        rslt[i] = s[i];
-        i++;
-    }
-	rslt[i] = '\n';
-    rslt[i+1] = '\0';  
-
-    return (rslt);
+		s_join[i] = cpy[i];
+		i++;
+	}
+	while (buff[j] != '\0')
+	{
+		s_join[i] = buff[j];
+		i++;
+		j++;
+	}
+	s_join[i] = '\0';
+	return (s_join);
 }
 
-char *ft_strchr(const char *s, int c) 
+char	*ft_strchr(const char *s, int c)
 {
-    int i = 0;
-    while (s[i]) 
+	int	i;
+
+	i = 0;
+	while (s[i])
 	{
-        if ((unsigned char)s[i] == (unsigned char)c)
-            return ((char *)&s[i]);
-        i++;
-    }
-    if ((unsigned char)c == '\0')
-        return ((char *)&s[i]);
-    return (NULL);
+		if ((unsigned char)s[i] == (unsigned char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if ((unsigned char)c == '\0')
+		return ((char *) &s[i]);
+	return (NULL);
+}
+
+
+int	ft_strlen(char *s)
+{
+	int i;
+	
+	i = 0;
+	while(s[i])
+		i++;
+	return (i);
 }
